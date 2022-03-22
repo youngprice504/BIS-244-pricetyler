722cab3ea6a5ee9e82eb0f40677ed892c26b1476
Troy Adair <tra418@lehigh.edu>
2022-03-21 20:57:29 -0400

    Assignment 2 graded

--- Assignment 2.R
+++ Assignment 2.R
@@ -1,8 +1,10 @@
 library(readr)
 library(dplyr)
-us_states <- read_csv("Desktop/Lehigh University Spring 2022/Business Analytics II/Assignment 2/BIS-244-pricetyler/covid-19-data/us-states.csv")
+# TAA You "hard-wired" reference to file
+us_states <- read_csv("us-states.csv")
 
 penn <- us_states %>% filter(state=="Pennsylvania")
 penn <- penn %>% mutate(incr_cases = cases - lag(cases)) %>% 
   mutate(incr_deaths = deaths - lag(deaths, order_by = date))
+# TAA Row 1 incr should be equal to row 1 cases/deaths
 print(penn %>% pull(incr_cases) %>% sd(na.rm = TRUE))
